08b1851684781d1594694b2a2bba120be7ca7637
Marcellus Githua <marcellusgithua@gmail.com>
2020-09-07 02:02:52 +0300

    add varibles

---
+++ pizza.js
@@ -0,0 +1,5 @@
+let destination = document.getElementById("home")
+let floor = document.getElementById("floor")
+let crust = document.getElementById("exampleRadios1").value
+let size = document.getElementById("size").value
+let toppings = document.getElementById("top").value
